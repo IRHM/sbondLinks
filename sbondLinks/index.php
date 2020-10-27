e78eff1114bc387c6c55bfef5d9da08948425dc7
IRHM <37304121+IRHM@users.noreply.github.com>
2020-10-27 19:01:30 +0000

    Use " instead of '
    Keep it more consistent instead of using either throughout

--- sbondLinks/index.php
+++ sbondLinks/index.php
@@ -1,9 +1,9 @@
 <?php
 $notice = "";
 
-if (isset($_GET['k']) && $link_key = $_GET['k']) {
+if (isset($_GET["k"]) && $link_key = $_GET["k"]) {
   // use link_key to get link and then redirect to the link
-  require('dbConnect.php');
+  require("dbConnect.php");
 
   // Declare vars that may not be later, but are needed
   $link = null;
@@ -12,7 +12,7 @@ if (isset($_GET['k']) && $link_key = $_GET['k']) {
   $stmt = mysqli_stmt_init($conn);
 
   if (!mysqli_stmt_prepare($stmt, $query)) {
-    echo 'Request failed. Please try again.';
+    echo "Request failed. Please try again.";
     exit();
   }
   else {
@@ -21,7 +21,7 @@ if (isset($_GET['k']) && $link_key = $_GET['k']) {
     $result = mysqli_stmt_get_result($stmt);
 
     while ($row = mysqli_fetch_assoc($result)) {
-      $link = $row['link'];
+      $link = $row["link"];
     }
   }
 
@@ -39,7 +39,7 @@ if (isset($_GET['k']) && $link_key = $_GET['k']) {
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <?php include('head.php'); ?>
+    <?php include("head.php"); ?>
   </head>
   <body>
     <script type="text/javascript" src="js/loading.js"></script>
@@ -54,14 +54,14 @@ if (isset($_GET['k']) && $link_key = $_GET['k']) {
         <div class="shortenLinkForm">
           <div class="linkGeneratedContainer">
             <span id="linkGenerated" class="" data-clipboard-target="#linkGenerated"><?= $notice ?></span>
-            <svg id="linkSvg" class='hidden' aria-hidden='true' focusable='false' data-prefix='fas' data-icon='copy' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512' width='15px' height='15px'>
-              <path fill='currentColor' d='M320 448v40c0 13.255-10.745 24-24 24H24c-13.255
+            <svg id="linkSvg" class="hidden" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="copy" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="15px" height="15px">
+              <path fill="currentColor" d="M320 448v40c0 13.255-10.745 24-24 24H24c-13.255
                 0-24-10.745-24-24V120c0-13.255 10.745-24 24-24h72v296c0
                 30.879 25.121 56 56 56h168zm0-344V0H152c-13.255 0-24
                 10.745-24 24v368c0 13.255 10.745 24 24 24h272c13.255
                 0 24-10.745 24-24V128H344c-13.2
                 0-24-10.8-24-24zm120.971-31.029L375.029 7.029A24 24 0 0 0 358.059
-                0H352v96h96v-6.059a24 24 0 0 0-7.029-16.97z'></path>
+                0H352v96h96v-6.059a24 24 0 0 0-7.029-16.97z"></path>
             </svg>
 
             <div id="linkSpinner" class="spinner hidden"></div>
--- sbondLinks/shortenLink.php
+++ sbondLinks/shortenLink.php
@@ -1,12 +1,12 @@
 <?php
-  require('dbConnect.php');
+  require("dbConnect.php");
 
   // Get JSON from POST request
-  $JSON = @file_get_contents('php://input');
+  $JSON = @file_get_contents("php://input");
 
   // If no JSON then exit
   if (!is_object(json_decode($JSON))) {
-    echo json_encode(array('error' => 'invalid request'));
+    echo json_encode(array("error" => "invalid request"));
     exit();
   }
 
@@ -14,9 +14,9 @@
   $linkArgs = json_decode($JSON, true);
 
   function generateRandomString($length = 5) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     $charactersLength = strlen($characters);
-    $randomString = '';
+    $randomString = "";
 
     for ($i = 0; $i < $length; $i++) {
       $randomString .= $characters[rand(0, $charactersLength - 1)];
@@ -25,13 +25,13 @@
     return $randomString;
   }
 
-  if (isset($linkArgs['link'])) {
+  if (isset($linkArgs["link"])) {
     // Get Link To Shorten from form
-    $linkToShorten = $linkArgs['link'];
+    $linkToShorten = $linkArgs["link"];
 
     // Add http:// if not already
-    $http     = 'http://';
-    $https    = 'https://';
+    $http     = "http://";
+    $https    = "https://";
     $hasHttp  = strpos($linkToShorten, $http);
     $hasHttps = strpos($linkToShorten, $https);
 
@@ -43,20 +43,20 @@
     }
 		
 		// check and set expiry date
-    if (isset($linkArgs['expiry']) && !empty($linkArgs['expiry'])) {
-      $expiryDate = $linkArgs['expiry'];
+    if (isset($linkArgs["expiry"]) && !empty($linkArgs["expiry"])) {
+      $expiryDate = $linkArgs["expiry"];
 
       switch ($expiryDate) {
-        case 'A Day':
+        case "A Day":
           $expire = "A Day";
           break;
-        case 'A Week':
+        case "A Week":
           $expire = "A Week";
           break;
-        case 'A Month':
+        case "A Month":
           $expire = "A Month";
           break;
-        case 'Never':
+        case "Never":
           $expire = "Never";
           break;
         default:
@@ -65,7 +65,7 @@
       }
     }
     else {
-      // Set expire to 'A Week' encase expiry isn't set
+      // Set expire to "A Week" encase expiry isn't set
       $expire = "A Week";
     }
 
@@ -75,7 +75,7 @@
     // Execute SQL Query
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $query)) {
-      echo json_encode(array('error' => 'backend error'));
+      echo json_encode(array("error" => "backend error"));
     }
     else {
       mysqli_stmt_bind_param($stmt, "ss", $linkToShorten, $expire);
@@ -83,7 +83,7 @@
       $result = mysqli_stmt_get_result($stmt);
 
       while ($row = mysqli_fetch_assoc($result)) {
-        $link_key = $row['link_key'];
+        $link_key = $row["link_key"];
       }
 
       $rowsSelected = mysqli_num_rows($result);
@@ -95,7 +95,7 @@
        * has already been shortened before.
        * If it has.. just get the same url & key to avoid duplication.
        */
-      echo json_encode(array('linkKey' => $link_key));
+      echo json_encode(array("linkKey" => $link_key));
     }
     else {
       // if link hasn't been shortened before then run this code (to shorten):
@@ -108,7 +108,7 @@
       // execute sql query
       $stmt = mysqli_stmt_init($conn);
       if (!mysqli_stmt_prepare($stmt, $sql)) {
-        echo json_encode(array('error' => 'backend error'));
+        echo json_encode(array("error" => "backend error"));
       }
       else {
         mysqli_stmt_bind_param($stmt, "sss", $link_key, $linkToShorten, $expire);
@@ -116,7 +116,7 @@
       }
 
       // Return links key
-      echo json_encode(array('linkKey' => $link_key));
+      echo json_encode(array("linkKey" => $link_key));
     }
 
     // Close Connection
